c7396ddb779e099cf8647d053ae9a0db24622df4
Sue Folkerts <suefolkerts@yahoo.com>
2021-04-25 17:37:01 -0700

    Deployed!

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FlaskWebProject1',
+    version='',
+    packages=[''],
+    url='RandomArtFun.com',
+    license='',
+    author='Suzie',
+    author_email='suefolkerts@yahoo.com',
+    description=''
+)
